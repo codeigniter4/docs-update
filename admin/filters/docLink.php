690631f89e0342856c0a58c3345199385d5a3bb6
michalsn <michal@sniatala.pl>
2023-12-23 19:59:58 +0100

    update docLink

--- admin/filters/docLink.php
+++ admin/filters/docLink.php
@@ -1,16 +1,75 @@
 <?php
 
 function before_filter_docLink($data, $folder) {
-    return preg_replace_callback('/:doc:`(.*?) <(.*?)>`/', function ($matches) {
+    // We only need a parent folder
+    $folder = explode('/', trim($folder,  '/'));
+    $folder = array_pop($folder);
+
+    // :doc:`View Decorators <../outgoing/view_decorators>`
+    // :doc:`Feature Tests </testing/feature>`
+    // :doc:`configuration <configuration>`
+    $data = preg_replace_callback('/:doc:`(.*?) <(.*?)>`/', function ($matches) use ($folder) {
         $text = $matches[1];
         $url  = $matches[2];
 
-        // Check if the URL doesn't contain '..'
-        if (! str_contains($url, '..')) {
-            $url = explode('/', $url);
-            $url = array_pop($url);
+        if (identifyRelativePath($url) === '../') {
+            return "`$text <$url.md>`_";
+        }
+
+        if (identifyRelativePath($url) === './') {
+            $url = str_replace('./', '', $url);
+            return "`$text <$url.md>`_";
+        }
+
+        if (identifyRelativePath($url) === '/') {
+            if (str_contains($url, $folder)) {
+                $url = explode('/', $url);
+                $url = array_pop($url);
+            } else {
+                $url = '..' . $url;
+            }
         }
 
         return "`$text <$url.md>`_";
     }, $data);
+
+    // :doc:`../outgoing/view_decorators`
+    // :doc:`./backward_compatibility_notes`
+    return preg_replace_callback('/:doc:`(.*?)`/', function ($matches) use ($folder) {
+        $url  = $matches[1];
+        $text = explode('/', $url);
+        $text = ucwords(str_replace('_', ' ', array_pop($text)));
+
+        if (identifyRelativePath($url) === '../') {
+            return "`$text <$url.md>`_";
+        }
+
+        if (identifyRelativePath($url) === './') {
+            $url = str_replace('./', '', $url);
+            return "`$text <$url.md>`_";
+        }
+
+        if (identifyRelativePath($url) === '/') {
+            if (str_contains($url, $folder)) {
+                $url = explode('/', $url);
+                $url = array_pop($url);
+            } else {
+                $url = '..' . $url;
+            }
+        }
+
+        return "`$text <$url.md>`_";
+    }, $data);
+}
+
+// helper function
+function identifyRelativePath($input)
+{
+    $pattern = '/^(\.\.\/|\.\/|\/)/';
+
+    if (preg_match($pattern, $input, $matches) === 1) {
+        return $matches[1];
+    }
+
+    return false;
 }
