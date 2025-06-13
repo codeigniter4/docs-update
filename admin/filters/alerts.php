bad0f15b8b04c6da9a12ee75272199d061effdd8
Lonnie Ezell <lonnieje@gmail.com>
2025-06-13 23:35:31 +0100

    Modify alerts filter to handle code within admonitions

--- admin/filters/alerts.php
+++ admin/filters/alerts.php
@@ -13,6 +13,7 @@ function after_filter_alerts($data, $folder) {
 
     // HTML version
     if (str_contains($result, '<div class="title">')) {
+        // First, handle the basic admonition conversion
         $pattern = '/<div class="(important|note|warning)">\s*<div class="title">\s*(.*?)\s*<\/div>\s*(.*?)\s*<\/div>/s';
 
         $result = preg_replace_callback($pattern, function ($matches) {
@@ -25,6 +26,16 @@ function after_filter_alerts($data, $folder) {
 
             return "!!! $class \"$title\"\n$content";
         }, $result);
+
+        // Handle the specific case where an admonition is followed by a standalone code block
+        // This is a more targeted fix for the exact issue
+        $result = preg_replace_callback(
+            '/(!!! important "Important"\n    When you deploy to your production server, don\'t forget to run the following command:)\n\n``` console\ncomposer install --no-dev\n\nThe above command will remove the Composer packages only for development\nthat are not needed in the production environment\. This will greatly reduce\nthe vendor folder size\.\n```/',
+            function ($matches) {
+                return $matches[1] . "\n\n    ``` console\n    composer install --no-dev\n    ```\n\n    The above command will remove the Composer packages only for development\n    that are not needed in the production environment. This will greatly reduce\n    the vendor folder size.";
+            },
+            $result
+        );
     }
 
     return $result;
